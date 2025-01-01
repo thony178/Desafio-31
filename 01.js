ac18f2b8043f8c82ee5038ae0412f16d2301ee60
Anthony <amthonychagasztta@gmail.com>
2025-01-01 20:32:18 -0300

    01 primeiro desafio check.

---
+++ 01.js
@@ -0,0 +1,44 @@
+// Desafio 31 dias com Erick Scur
+
+//Dia 1 - Variáveis
+// O que é? E quais os tipos?
+
+// Existe três tipos de variavel dentro do JS a const, let e var
+// Const é um valor fixo que nunca muda, imutavel
+// let é um valor mutavel
+// var é un jeito antigo então usei menos
+
+// Dica: Sempre depois de uma linha de código feita coloque ; para não ter dor de cabeça depois
+
+let nome = "Anthony"
+
+let cidade = "Rio de janeiro"
+
+nome = "Thony"
+
+cidade = "São paulo"
+
+console.log("Olá meu nome é",nome,"e moro em",cidade)
+
+//Tipos de dados
+
+//String
+let name = "String é tipo de texto"
+
+//Number não tem diferencia do decimal para inteiro
+let number = 70
+
+//boolean
+let maiorIdade = true
+
+//Array
+let lista = ["A, b , B"]
+
+//Objeto
+let pessoa = {
+    nome: Anthony
+    idade: 18
+    Cidade: São paulo
+}
+
+pessoa.idade // console.log vai mostrar só a idade dentro do objeto
