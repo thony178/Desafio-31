930cfef978bea033885cb78ee77a8fc38a02cc69
Anthony Chagas Zatta <amthonychagaszatta@gmail.com>
2025-06-21 10:38:00 -0300

    Iniciando o desafio 31 de programação. por Erick Scu

---
+++ dia1.js
@@ -0,0 +1,37 @@
+// Dia 1 desafio 31 - Variáveis
+
+// O que é variavel?
+
+let nome = "Anthony";
+
+let cidade;
+cidade = "São paulo";
+
+console.log("Olá meu nome é", nome, "e eu moro em", cidade);
+
+// Quais os tipos de dados?
+
+//    Strings
+let Nome = "Anthony";
+
+//    Number
+let Number = 70.5;
+Number = 50;
+
+//    Boolean (True = verdade/ false = falso )
+let maiorDidade = true;
+maiorDidade = false;
+
+//    Array (Array é qualquer tipo de lista)
+let listaNumber = [1, 2, 3, 5];
+let listaNome = ["Anthony", "Pedro"];
+let listaBoolean = [true, false, false];
+
+//   Objeto
+let pessoa = {
+    Nome: "Anthony",
+    Cidade: "São paulo",
+    idade: 19
+}
+
+console.log(pessoa.Nome);
---
+++ test.js
@@ -0,0 +1,2 @@
+let dev = "Um dia chego lá!!!";
+const devJ = "1% todo dia eu chego lá";
